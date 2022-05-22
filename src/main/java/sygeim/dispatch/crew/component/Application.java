87431dff56471f59bfad8837a8a546609716ba54
Elliot <jopichardomartinez@gmail.com>
2022-05-22 01:34:18 -0400

    Updated Application.java

--- src/main/java/sygeim/dispatch/crew/component/Application.java
+++ src/main/java/sygeim/dispatch/crew/component/Application.java
@@ -1,7 +1,14 @@
 package sygeim.dispatch.crew.component;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "sygeim.dispatch.crew.component"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
